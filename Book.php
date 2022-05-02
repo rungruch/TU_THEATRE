20dc9eb153516478da1efae6d7c08151b82344fb
rungruch <rungruch2543@gmail.com>
2022-05-02 20:54:40 +0700

    Update Book.php

--- Book.php
+++ Book.php
@@ -50,6 +50,7 @@ $_SESSION['RemainingSeat']= $RemainingSeat;
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
+    <title>จองภาพยนตร์</title>
     <link rel="icon" href="img/ico.ico" type="image/ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit&amp;display=swap">
@@ -65,6 +66,12 @@ $_SESSION['RemainingSeat']= $RemainingSeat;
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script>
       window.addEventListener('load', showprice);
+
+ 
+    if(typeof window.history.pushState == 'function') {
+        window.history.pushState({}, "Hide", "http://localhost/tutheatre/Book.php?");
+    }
+
       
       var price = " <?php echo  $Price  ?> ";
   
